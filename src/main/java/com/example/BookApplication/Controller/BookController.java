25323784b2d1deec6d08922e8533044be0a4439f
omkar1103 <115880514+omkar1103@users.noreply.github.com>
2025-11-18 01:04:00 +0530

    Post API

---
+++ src/main/java/com/example/BookApplication/Controller/BookController.java
@@ -0,0 +1,28 @@
+package com.example.BookApplication.Controller;
+
+import com.example.BookApplication.Entity.Book;
+import com.example.BookApplication.Service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("book/V1")
+public class BookController {
+
+    private final BookService bookService;
+    @Autowired
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @PostMapping("/addBook")
+    public ResponseEntity<Book> addBook(@RequestBody Book book){
+       Book savbook= bookService.addBook(book);
+       return ResponseEntity.ok(savbook);
+
+    }
+}
